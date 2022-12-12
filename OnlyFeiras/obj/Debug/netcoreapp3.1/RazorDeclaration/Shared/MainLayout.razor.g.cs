d5dca759076b6792a4863f4020c6ddfa2450b424
Gustavo Pereira <73430054+cgustavop@users.noreply.github.com>
2022-12-12 16:23:08 +0000

    Add CardFeira component (#4)

--- OnlyFeiras/obj/Debug/netcoreapp3.1/RazorDeclaration/Shared/MainLayout.razor.g.cs
+++ OnlyFeiras/obj/Debug/netcoreapp3.1/RazorDeclaration/Shared/MainLayout.razor.g.cs
@@ -13,63 +13,63 @@ namespace OnlyFeiras.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "D:\github\OnlyFeiras\OnlyFeiras\_Imports.razor"
+#line 1 "C:\Users\Nitro\dev\OnlyFeiras\OnlyFeiras\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "D:\github\OnlyFeiras\OnlyFeiras\_Imports.razor"
+#line 2 "C:\Users\Nitro\dev\OnlyFeiras\OnlyFeiras\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\github\OnlyFeiras\OnlyFeiras\_Imports.razor"
+#line 3 "C:\Users\Nitro\dev\OnlyFeiras\OnlyFeiras\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "D:\github\OnlyFeiras\OnlyFeiras\_Imports.razor"
+#line 4 "C:\Users\Nitro\dev\OnlyFeiras\OnlyFeiras\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "D:\github\OnlyFeiras\OnlyFeiras\_Imports.razor"
+#line 5 "C:\Users\Nitro\dev\OnlyFeiras\OnlyFeiras\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "D:\github\OnlyFeiras\OnlyFeiras\_Imports.razor"
+#line 6 "C:\Users\Nitro\dev\OnlyFeiras\OnlyFeiras\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "D:\github\OnlyFeiras\OnlyFeiras\_Imports.razor"
+#line 7 "C:\Users\Nitro\dev\OnlyFeiras\OnlyFeiras\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "D:\github\OnlyFeiras\OnlyFeiras\_Imports.razor"
+#line 8 "C:\Users\Nitro\dev\OnlyFeiras\OnlyFeiras\_Imports.razor"
 using OnlyFeiras;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "D:\github\OnlyFeiras\OnlyFeiras\_Imports.razor"
+#line 9 "C:\Users\Nitro\dev\OnlyFeiras\OnlyFeiras\_Imports.razor"
 using OnlyFeiras.Shared;
 
 #line default
@@ -78,7 +78,7 @@ using OnlyFeiras.Shared;
     public partial class MainLayout : LayoutComponentBase
     {
         #pragma warning disable 1998
-        protected override void BuildRenderTree(global::Microsoft.AspNetCore.Components.Rendering.RenderTreeBuilder __builder)
+        protected override void BuildRenderTree(Microsoft.AspNetCore.Components.Rendering.RenderTreeBuilder __builder)
         {
         }
         #pragma warning restore 1998
