af174acc942bc101832c8d984915d24ff86d6012
Gustavo Pereira <73430054+cgustavop@users.noreply.github.com>
2022-12-28 07:52:50 +0000

    ➕ Classes and migrations (#7)

---
+++ webOnlyFeiras/webOnlyFeiras/Model/Banca.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace webOnlyFeiras.Model
+{
+    public class Banca
+    {
+        [Key]
+        public int BancaId { get; set; }
+        [Required]
+        public string PathFotografia { get; set; }
+        public string Descricao { get; set; }
+
+    }
+}
---
+++ webOnlyFeiras/webOnlyFeiras/Model/Utilizador.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace webOnlyFeiras.Model
+{
+    public class Utilizador
+    {
+        [Key]
+        public int ID { get; set; }
+        
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public DateTime DataNascimento { get; set; }
+        public string Morada { get; set; }
+
+        public int Contacto { get; set;}
+
+        [Required]
+        public string Password { get; set; }
+
+        public int PathFotografiaPerfil { get; set; }
+        
+        [Required]
+        public bool IsOrganizador { get; set; }
+
+
+    }
+}
